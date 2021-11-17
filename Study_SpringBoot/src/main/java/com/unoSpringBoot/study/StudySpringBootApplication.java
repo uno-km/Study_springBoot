505fa7be414f935753b702af69137b3053659e22
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-17 22:06:50 +0900

    initializing Commit

---
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/StudySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.unoSpringBoot.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringBootApplication.class, args);
+	}
+
+}
