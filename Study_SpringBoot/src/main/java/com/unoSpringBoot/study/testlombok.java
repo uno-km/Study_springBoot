0acf83960c88482e2318168a450dd73be95ef459
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-18 22:25:42 +0900

    빌드추가, 롬복 테스트완료

---
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/testlombok.java
@@ -0,0 +1,13 @@
+package com.unoSpringBoot.study;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class testlombok {
+
+	@NonNull
+	private String lombok;
+}
