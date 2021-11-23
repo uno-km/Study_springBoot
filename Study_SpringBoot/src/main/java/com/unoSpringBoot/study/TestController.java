cfb72838753afaaeea15382677ec58e706d8e45f
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-23 23:29:06 +0900

    리스폰스 바디를 이용해 오브젝트 객체를 출력하는 방법을 실습

--- Study_SpringBoot/src/main/java/com/unoSpringBoot/study/TestController.java
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/TestController.java
@@ -1,10 +1,17 @@
 package com.unoSpringBoot.study;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.unoSpringBoot.study.DTO.ResponseDTO;
+import com.unoSpringBoot.study.DTO.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test")
@@ -27,5 +34,18 @@ public class TestController {
 	@GetMapping("/testRequestParam")
 	public String testControllerRequestParam(@RequestParam(required = false) String id) {
 		return "Hello wolrd!! ID = " + id + "하위~~!!";
+	}
+
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello Wolrd!!!! name " + testRequestBodyDTO.getName() + " 그리고 나이는 " + testRequestBodyDTO.getAge();
+	}
+
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponsBody() {
+		List<String> list = new ArrayList<String>();
+		list.add("하위~~~~ 내이름은 ");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
 	}
 }
