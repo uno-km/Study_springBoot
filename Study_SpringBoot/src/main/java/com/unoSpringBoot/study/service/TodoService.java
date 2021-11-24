c3d061912d7e308eb6535829ed52a8999d478249
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-24 23:01:24 +0900

    비즈니스로직 테스트 성공

--- Study_SpringBoot/src/main/java/com/unoSpringBoot/study/service/TodoService.java
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/service/TodoService.java
@@ -1,10 +1,22 @@
 package com.unoSpringBoot.study.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.unoSpringBoot.study.model.TodoEntity;
+import com.unoSpringBoot.study.persistence.TodoRepository;
 
 @Service
 public class TodoService {
+
+	@Autowired
+	private TodoRepository todoRepository;
+
 	public String testService() {
-		return "TEST SERVICE";
+		TodoEntity todoEntity = TodoEntity.builder().title("하위~").userId("KimEunHo").build();
+		todoRepository.save(todoEntity);
+		TodoEntity saveEntity = todoRepository.findById(todoEntity.getId()).get();
+		return saveEntity.getUserId()+" <-- 아이디 , 제목 --> " + saveEntity.getTitle();
 	}
+
 }
