e2c8d3c7dc7bfbcebea10ac4b341b45103fb0f24
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-24 22:09:58 +0900

    엔티티에 기본값추가

--- Study_SpringBoot/src/main/java/com/unoSpringBoot/study/Controller/TodoController.java
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/Controller/TodoController.java
@@ -3,16 +3,22 @@ package com.unoSpringBoot.study.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unoSpringBoot.study.DTO.ResponseDTO;
+import com.unoSpringBoot.study.service.TodoService;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+
+	@Autowired
+	private TodoService service;
+
 	@GetMapping("/badConnect")
 	public ResponseEntity<?> testControllerResponseEntity() {
 		List<String> list = new ArrayList<String>();
@@ -20,4 +26,14 @@ public class TodoController {
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.badRequest().body(response);
 	}
+
+	@GetMapping("/test")
+	public ResponseEntity<ResponseDTO<String>> testTodo() {
+		String str = service.testService();
+		List<String> list = new ArrayList<String>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+
 }
--- Study_SpringBoot/src/main/java/com/unoSpringBoot/study/model/TodoEntity.java
+++ Study_SpringBoot/src/main/java/com/unoSpringBoot/study/model/TodoEntity.java
@@ -1,5 +1,9 @@
 package com.unoSpringBoot.study.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+//@Entity에 이름을 부여할수있다. @Entity("TodoEntity)
+@Table(name = "Todo")
 public class TodoEntity {
+	@Id
 	private String id; // 오브젝트의 아이디
-	private String userId; //이 오브젝를 생성한 아이디
-	private String title; //Todo타이틀
-	private boolean done; //true - todo 를 완료 , 미완료했을때
-	
+	private String userId; // 이 오브젝를 생성한 아이디
+	private String title; // Todo타이틀
+	private boolean done; // true - todo 를 완료 , 미완료했을때
+
 }
