686b2ef3fe2efc9a3daa5ccea96b721584adcb5d
zhfld <zhfld@LAPTOP-AFG5HTSP>
2021-12-13 23:29:08 +0900

    :sparkles:Introduce new Features

---
+++ Study_SpringBoot/src/main/webapp/react-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './js/App.js';
+import reportWebVitals from './js/reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
