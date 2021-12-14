90e964d4328a7600fdfac06b6eac4182c2313a31
zhfld <zhfld@LAPTOP-AFG5HTSP>
2021-12-15 00:02:35 +0900

    update to react

--- Study_SpringBoot/src/main/webapp/src/js/service/ApiService.js
+++ Study_SpringBoot/src/main/webapp/src/js/service/ApiService.js
@@ -2,18 +2,19 @@ import { API_BASE_URL } from "../api-config";
 
 
 export function call(api, method, request) {
-    let options = {
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      url: API_BASE_URL + api,
-      method: method,
-    };
-    if (request) {
-      // GET method
-      options.body = JSON.stringify(request);
-    }
-    return fetch(options.url, options).then((response) =>
+  let options = {
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    url: API_BASE_URL + api,
+    method: method,
+  };
+  if (request) {
+    // GET method
+    options.body = JSON.stringify(request);
+  }
+  return fetch(options.url, options)
+    .then((response) =>
       response.json().then((json) => {
         if (!response.ok) {
           // response.ok가 true이면 정상적인 리스폰스를 받은것, 아니면 에러 리스폰스를 받은것.
@@ -21,5 +22,13 @@ export function call(api, method, request) {
         }
         return json;
       })
-    );
-  }
+    )
+    .catch((error) => {
+      // 추가된 부분
+      console.log(error.status);
+      if (error.status === 403) {
+        window.location.href = "/login"; // redirect
+      }
+      return Promise.reject(error);
+    });
+}
